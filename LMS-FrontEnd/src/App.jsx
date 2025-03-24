982a9d6b5af9849ecea02686b8ca7db442bb85ed
ravikumar50 <ravikumarsingh5256@gmail.com>
2025-03-24 16:18:22 +0530

    added footer

--- LMS-FrontEnd/src/App.jsx
+++ LMS-FrontEnd/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
 import { Routes } from "react-router-dom";
+import Footer from "./Components/footer";
 
 function App() {
   return (
     <>
-      <Routes></Routes>
+      <Footer/>
     </>
   );
 }
--- LMS-FrontEnd/src/main.jsx
+++ LMS-FrontEnd/src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
-
-import store from "../Redux/store.js";
+import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "./react-redux";
+import { Provider } from "react-redux";
+import store from "./Redux/store.js";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
