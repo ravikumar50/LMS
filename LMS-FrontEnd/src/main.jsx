121437939c04698f6df443fea9e6b150d7cbde16
ravikumar50 <ravikumarsingh5256@gmail.com>
2025-03-24 13:26:04 +0530

    started adding basic redux setup

--- LMS-FrontEnd/src/main.jsx
+++ LMS-FrontEnd/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 
+import store from "../Redux/store.js";
 import App from "./App.jsx";
+import { Provider } from "./react-redux";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-    <Toaster></Toaster>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster></Toaster>
+    </BrowserRouter>
+  </Provider>,
 );
