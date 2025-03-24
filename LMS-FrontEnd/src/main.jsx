ff3fde54180be4e9f5b310441c8cb90f6d0bed95
ravikumar50 <ravikumarsingh5256@gmail.com>
2025-03-24 13:08:00 +0530

    added react toater

--- LMS-FrontEnd/src/main.jsx
+++ LMS-FrontEnd/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
-
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster></Toaster>
   </BrowserRouter>,
 );
